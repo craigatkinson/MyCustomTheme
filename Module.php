cf03bc37c8ef873f960de48611f47c2169bfcb09
nigel lundsten <nigel. lundsten@gmail.com>
2012-11-26 14:43:02 -0700

    provide layout based on controller

--- Module.php
+++ Module.php
@@ -2,8 +2,19 @@
 
 namespace MyCustomTheme;
 
-class Module
+use Zend\ModuleManager\Feature;
+use Zend\EventManager\EventInterface;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteMatch;
+
+class Module implements
+    Feature\AutoloaderProviderInterface,
+    Feature\ConfigProviderInterface,
+    Feature\ServiceProviderInterface,
+    Feature\BootstrapListenerInterface
 {
+    protected $serviceConfig;
+
     public function getAutoloaderConfig()
     {
         return array(
@@ -15,18 +26,58 @@ class Module
         );
     }
 
+    public function onBootstrap(EventInterface $e)
+    {
+        $app = $e->getParam('application');
+        $em  = $app->getEventManager();
+
+        $em->attach(MvcEvent::EVENT_DISPATCH, array($this, 'layoutForRoute'));
+    }
+
     public function getConfig()
     {
-        $config = array(
-            'view_manager' => array(
-                'template_map' => array(
-                    'layout/layout' => __DIR__ . '/src/' . __NAMESPACE__ . '/view/layout/layout.phtml',
-                ),
-                'template_path_stack' => array(
-                    __DIR__ . '/src/' . __NAMESPACE__ . '/view',
-                ),
-            ),
+        $config =  array();
+        $configFiles = array(
+            __DIR__ . '/config/module.config.php',
         );
+        foreach($configFiles as $configFile) {
+            $config = \Zend\Stdlib\ArrayUtils::merge($config, include $configFile);
+        }
+
         return $config;
+    }
+
+    public function layoutForRoute(MvcEvent $e)
+    {
+        $app    = $e->getParam('application');
+        $sm     = $app->getServiceManager();
+        $config = $sm->get('config');
+
+        $match = $e->getRouteMatch();
+        if ($match instanceof RouteMatch && $match->getParam('controller') === 'catalog') {
+            $layout     = $config['my_custom_theme']['catalog_layout'];
+            $controller = $e->getTarget();
+            $controller->layout($layout);
+        } else {
+            return;
+        }
+    }
+
+    /**
+     * @return serviceConfig
+     */
+    public function getServiceConfig()
+    {
+        return $this->serviceConfig;
+    }
+
+    /**
+     * @param $serviceConfig
+     * @return self
+     */
+    public function setServiceConfig($serviceConfig)
+    {
+        $this->serviceConfig = $serviceConfig;
+        return $this;
     }
 }
---
+++ config/module.config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'my_custom_theme' => array(
+        'catalog_layout' => 'layout/my-custom-layout',
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view'
+        ),
+    ),
+);
