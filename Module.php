0a8449707975c5489dd646ddf1598e50a62d213c
nigel lundsten <nigel. lundsten@gmail.com>
2012-11-26 12:28:27 -0700

    add files

---
+++ Module.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MyCustomTheme;
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getViewHelperConfig()
+    {
+        return array(
+        );
+    }
+
+    public function getConfig()
+    {
+        $config = array(
+            'view_manager' => array(
+                'template_map' => array(
+                    'layout/layout' => __DIR__ . '/' . '/src/' . __NAMESPACE__ . '/view/layout/layout.phtml',
+                ),
+                'template_path_stack' => array(
+                    __DIR__ . '/' . '/src/' . __NAMESPACE__ . '/view',
+                ),
+            ),
+        );
+        return $config;
+    }
+}
